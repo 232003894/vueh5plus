373b88c7061037ac03749e4a20cfe9cc24fb852c
232003894 <232003894@qq.com>
2018-03-02 10:28:56 +0800

    文档修改

--- src/lib/plus/os.js
+++ src/lib/plus/os.js
@@ -6,7 +6,7 @@
 const ua = navigator.userAgent
 
 let _os = {
-  name: 'web',
+  name: "web",
   version: 0,
   wechat: false,
   android: false,
@@ -21,46 +21,45 @@ let _os = {
 // wechat
 let match = ua.match(/(MicroMessenger)\/([\d.]+)/i)
 if (match) {
-  _os.name += ' wechat'
-  _os.wechat = match[2].replace(/_/g, '.')
+  _os.name += " wechat"
+  _os.wechat = match[2].replace(/_/g, ".")
 }
 // android
 match = ua.match(/(Android);?[\s/]+([\d.]+)?/)
 if (match) {
-  _os.name += ' android'
+  _os.name += " android"
   _os.android = true
   _os.version = match[2]
-  _os.isBadAndroid = !(/Chrome\/\d/.test(window.navigator.appVersion))
+  _os.isBadAndroid = !/Chrome\/\d/.test(window.navigator.appVersion)
 }
 // ipad
 match = ua.match(/(iPad).*OS\s([\d_]+)/)
 if (match) {
-  _os.name += ' iPad'
+  _os.name += " iPad"
   _os.ios = _os.ipad = true
-  _os.version = match[2].replace(/_/g, '.')
+  _os.version = match[2].replace(/_/g, ".")
 }
 // iphone
 match = ua.match(/(iPhone\sOS)\s([\d_]+)/)
 if (!_os.ipad && match) {
-  _os.name += ' iPhone'
+  _os.name += " iPhone"
   _os.ios = _os.iphone = true
-  _os.version = match[2].replace(/_/g, '.')
+  _os.version = match[2].replace(/_/g, ".")
 }
 //  5+ Browser
 match = ua.match(/Html5Plus/i)
 if (match) {
-  _os.name += ' h5+'
+  _os.name += " h5+"
   _os.plus = true
   // document.body.classList.add( $.className( 'plus' ) )
 }
 // 最好有流应用自己的标识
 match = ua.match(/StreamApp/i)
 if (match) {
-  _os.name += ' stream'
+  _os.name += " stream"
   _os.stream = true
   // document.body.classList.add( $.className( 'plus-stream' ) )
 }
-
 
 /**
  * 浏览器属性
@@ -72,7 +71,7 @@ if (match) {
  * @property {Boolean} ios 是否ios系统
  * @property {Boolean} ipad 是否ipad
  * @property {Boolean} iphone 是否iphone
- * @property {Boolean} iphone 是否H5+
+ * @property {Boolean} plus 是否H5+
  * @property {Boolean} stream 是否stream
  */
-export let os = _os;
+export let os = _os
