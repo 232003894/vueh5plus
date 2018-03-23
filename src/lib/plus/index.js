392ce212611ed9c5770a4950592a1e0643bd4bc3
232003894 <232003894@qq.com>
2018-03-23 17:31:35 +0800

    fix

--- src/lib/plus/index.js
+++ src/lib/plus/index.js
@@ -1,9 +1,10 @@
 // fixed
-import '../common/_fixed'
-export * from '../common/utils'
-export * from './os'
-export * from './broadcast'
-export * from './nativeUI'
-export * from './window'
-export * from './navigator'
-export * from './back'
+import "../common/_fixed"
+export * from "../common/utils"
+export * from "./os"
+export * from "./broadcast"
+export * from "./nativeExt"
+export * from "./nativeUI"
+export * from "./window"
+export * from "./navigator"
+export * from "./back"
